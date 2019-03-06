fa79f2f81e84f22cba303d4464a3a7657c869f4c
NuttySalmon <nuttysalmon@gmail.com>
2019-03-05 19:23:23 -0800

    Init. Impliment launching executable with arguments.

--- test.c
+++ test.c
@@ -1,30 +1,51 @@
-#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int
-main(int argc, char **argv)
+main(int argc, char *argv[])
 {
+    FILE *stream;
     char *line = NULL;
-    size_t linesize = 0;
-    ssize_t linelen;
+    size_t len = 0;
+    ssize_t nread;
+    const char *path = "./";
 
-    while ((linelen = getline(&line, &linesize, stdin)) != -1) {
-    	if (strncmp("exit", line, 4) == 0) {
-    	// user wants to exit
-    	  exit(0);
-    }
 
-    fwrite(line, linelen, 1, stdout);
+    while ((nread = getline(&line, &len, stdin)) != -1) {
 
-    	// fork
-    	// if child, exec
-    	// if parent, wait
-    }
+        if(strcmp(line, "exit\n") == 0){
+            exit(EXIT_SUCCESS);
+        }
+
+        int child = fork();
+
+        char *arg[3];
+
+        int counter = 0;
+        char * split = strtok(line, "\n ");
+        
+
+        while(counter <= 3&& split != NULL){
+            arg[counter] = strdup(split);
+            //printf("%s", split);
+            split = strtok (NULL, "\n ");
+            counter++;
+        }
+        arg[counter] = NULL;
+
+        if(child == 0){
+            execv(arg[0], arg);
+        }
+        else{
+            int wc = wait(NULL);
+        }
+        
+        
+   }
 
     free(line);
-    if (ferror(stdin))
-    err(1, "getline");
-
+    fclose(stream);
+    exit(EXIT_SUCCESS);
 }
