3bfff3dd2469dcb497118b7f01a9568fd528cde3
Alwin Riady argono <alwinriadyargono@Alwins-MacBook-Pro.local>
2019-03-09 15:06:13 -0800

    path

--- test.c
+++ test.c
@@ -3,6 +3,69 @@
 #include <string.h>
 #include <unistd.h>
 
+void tst_error()
+{
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message)); 
+    exit(1);
+}
+
+int changeDir(char** comand)
+{
+    char* cdCommand = "cd";
+    if(strcmp(comand[0], cdCommand) == 1)
+    {
+        if(comand[1] != NULL && comand[2] == NULL)
+        {
+            chdir(comand[1]);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+//Function to print current directory
+void printDirectory()
+{
+    char dir[1024];
+    getcwd(dir, sizeof(dir));
+    printf("\nDir: %s", dir);
+}
+
+char * tst_path(char *a[])
+{
+ char path[512];
+ char wholename[512];
+ char *next_piece;
+ int counter = 0;
+ int size = 0;
+ int bcount = 0;
+ 
+ //b = malloc(size, sizeof(string));
+
+while(a[counter] != NULL)
+{
+    counter++;
+}
+size = counter;
+char *b[size];
+counter = 0;
+
+ while(a[counter] != NULL)
+{
+    snprintf(path, 511, a[counter]);
+    if (access(path, X_OK) == 0) 
+    {
+        //realloc(b, size);
+        b[bcount] = a[counter];
+        bcount++;
+    }
+    counter++; 
+}
+ return b;
+}
+
+
 int
 main(int argc, char *argv[])
 {
@@ -55,28 +118,6 @@ main(int argc, char *argv[])
     free(line);
     fclose(stream);
     exit(EXIT_SUCCESS);
-}
-
-int changeDir(char** comand)
-{
-	char* cdCommand = "cd";
-	if(strcmp(comand[0], cdCommand) == 1)
-	{
-		if(comand[1] != NULL && comand[2] == NULL)
-		{
-			chdir(comand[1]);
-			return 1;
-		}
-	}
-	return 0;
-}
-
-//Function to print current directory
-void printDirectory()
-{
-	char dir[1024];
-	getcwd(dir, sizeof(dir));
-	printf("\nDir: %s", dir);
 }
 
 
